165c35e803108c26afd7a624ffff7c1b5bcf9373
Rohith <133040572+Rohith-repo@users.noreply.github.com>
2023-12-07 21:10:47 +0530

    Update Reverse.c

--- Basic Questions/Reverse.c
+++ Basic Questions/Reverse.c
@@ -4,15 +4,15 @@ int main()
     int N=1234;
     printf("%d ",Reverse(N));
 }
-int Reverse(int N,int digits)
+int Helper(int N,int digits)
 {
     if(N<10)
         return N;
     int Rem = N%10;
-    return Rem * (pow(10,digits-1)) + Reverse((N/10),digits-1);
+    return Rem * (pow(10,digits-1)) + Helper((N/10),digits-1);
 }
-int TotalDigits(int N)
+int Reverse(int N)
 {
     int digits = (int) log10(N) + 1;
-    return Reverse(N,digits);
+    return Helper(N,digits);
 }
